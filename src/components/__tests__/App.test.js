e37064673603f8f813d2016cc5837e66d0705d97
Kasia <kasia.skonecka@gmail.com>
2018-08-08 21:35:03 +0200

    first tests added

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+// import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+})
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // // looks inside the div and checks to see if the Comment Box is in there
+  // // console.log(div.innerHTML);
+  // expect(div.innerHTML).toContain('Comment Box')
+  // ReactDOM.unmountComponentAtNode(div)
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+})
 
-  ReactDOM.render(<App />, div);
-
-  // looks inside the div and checks to see if the Comment Box is in there
-  // console.log(div.innerHTML);
-  expect(div.innerHTML).toContain('Comment Box')
-
-  ReactDOM.unmountComponentAtNode(div)
+it('shows a comment list', () => {
+  // const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 })
