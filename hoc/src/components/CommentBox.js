a16d3ca5789b3b8d08a31a8e780d55c3e503e894
Kasia <kasia.skonecka@gmail.com>
2018-08-13 21:55:55 +0200

    added higher order component functionality

--- hoc/src/components/CommentBox.js
+++ hoc/src/components/CommentBox.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
+import requireAuth from 'components/requireAuth'
 
 class CommentBox extends Component {
   state = {
     comment: ''
   }
+
   handleChange = (event) => {
     this.setState({ comment: event.target.value})
   }
@@ -33,4 +35,5 @@ class CommentBox extends Component {
   }
 }
 
-export default connect(null, actions)(CommentBox);
+
+export default connect(null, actions)(requireAuth(CommentBox));
